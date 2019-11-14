361ca972bc2c5d3c2f7ea07a3d477b0ec1e1b9c3
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-14 20:09:24 +0300

    Add filter and order functionality

--- backend/couscous/v1/invoice/views.py
+++ backend/couscous/v1/invoice/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics, permissions
 
 import couscous.v1.invoice.models as invoice_models
 import couscous.v1.invoice.serializers as invoice_serializers
@@ -11,6 +12,9 @@ class ListInvoiceView(generics.ListAPIView):
     permission_classes = (permissions.IsAdminUser,)
     serializer_class = invoice_serializers.InvoiceSerializer
     queryset = invoice_models.Invoice.objects.all()
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filter_fields = ('debtor__email', 'status', 'amount', 'due_date')
+    ordering_fields = ['debtor__email', 'status', 'amount', 'due_date']
 
     def get_queryset(self):
         return self.queryset.filter(debtor__created_by=self.request.user)
