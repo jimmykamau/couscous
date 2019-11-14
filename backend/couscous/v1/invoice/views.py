370d1e2c4b196365599de81f6a568d59d683c9e1
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-14 20:03:36 +0300

    Create ListInvoiceView

--- backend/couscous/v1/invoice/views.py
+++ backend/couscous/v1/invoice/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+import couscous.v1.invoice.models as invoice_models
+import couscous.v1.invoice.serializers as invoice_serializers
+
+
+class ListInvoiceView(generics.ListAPIView):
+    """
+    List Invoices
+    """
+    permission_classes = (permissions.IsAdminUser,)
+    serializer_class = invoice_serializers.InvoiceSerializer
+    queryset = invoice_models.Invoice.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(debtor__created_by=self.request.user)
