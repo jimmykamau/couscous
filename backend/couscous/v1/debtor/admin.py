66de345c63a911bbf807f92d109c768a6d9ad59a
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-14 19:04:25 +0300

    Create DebtorAdmin and register model

--- backend/couscous/v1/debtor/admin.py
+++ backend/couscous/v1/debtor/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Debtor
+
+
+class DebtorAdmin(admin.ModelAdmin):
+    exclude = ['created_by']
+    list_display = (
+        'email',
+    )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(created_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj:
+            return True
+        if obj.created_by == request.user:
+            return True
+        return False
+
+    has_delete_permission = has_change_permission
+
+
+admin.site.register(Debtor, DebtorAdmin)
