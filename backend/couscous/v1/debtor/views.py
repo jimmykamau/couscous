ed273806ef864d92c92c2cd20793142ecf3f820c
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-14 19:41:41 +0300

    Create ListDebtorView

--- backend/couscous/v1/debtor/views.py
+++ backend/couscous/v1/debtor/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+import couscous.v1.debtor.models as debtor_models
+import couscous.v1.debtor.serializers as debtor_serializers
 
-# Create your views here.
+
+class ListDebtorView(generics.ListAPIView):
+    """
+    List debtors
+    """
+    permission_classes = (permissions.IsAdminUser,)
+    serializer_class = debtor_serializers.DebtorSerializer
+    queryset = debtor_models.Debtor.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(created_by=self.request.user)
